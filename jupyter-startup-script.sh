22f58a1c49078fc3143d7527c385137875580560
Vedmani <78539957+Vedmani@users.noreply.github.com>
2024-07-15 06:22:22 +0000

    chore: Update Dockerfile.main to and configure startup which hopefully is compatible with paperspace

---
+++ jupyter-startup-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Default command
+CMD="jupyter lab --allow-root --ip=0.0.0.0 --no-browser \
+     --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False \
+     --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' \
+     --ServerApp.allow_credentials=True --notebook-dir=/home/projects"
+
+# If a command is passed to the script, run that instead
+if [ $# -gt 0 ]; then
+    exec "$@"
+else
+    # Otherwise, run the default command
+    exec $CMD
+fi
