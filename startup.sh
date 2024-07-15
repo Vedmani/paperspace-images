0f4e857197c4c1812d5a2992b4334d7d74ce3ddf
Vedmani <78539957+Vedmani@users.noreply.github.com>
2024-07-15 06:35:45 +0000

    Update startup script to print debug information on which command is getting passed to it by paperspace. Need to build a v4 docker image after this change. v3 is working correctly before this commit

--- startup.sh
+++ startup.sh
@@ -6,10 +6,23 @@ CMD="jupyter lab --allow-root --ip=0.0.0.0 --no-browser \
      --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' \
      --ServerApp.allow_credentials=True --notebook-dir=/home/projects"
 
+# # If a command is passed to the script, run that instead
+# if [ $# -gt 0 ]; then
+#     exec "$@"
+# else
+#     # Otherwise, run the default command
+#     exec $CMD
+# fi
+
+# Print debug information
+print_debug_info "$@"
+
 # If a command is passed to the script, run that instead
 if [ $# -gt 0 ]; then
+    [ "$DEBUG" != "false" ] && echo "Executing provided command: $@"
     exec "$@"
 else
     # Otherwise, run the default command
+    [ "$DEBUG" != "false" ] && echo "Executing default command: $CMD"
     exec $CMD
 fi
